9e54fd03e24fa25a4f56bb5a0d2a3e5785e91a62
lianshufeng <251708339@qq.com>
2018-03-27 17:00:27 +0800

    修改默认启动配置

--- DHTSniffer-Daemon/src/main/resources/bootstrap.bat
+++ DHTSniffer-Daemon/src/main/resources/bootstrap.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-java -Dname=Deamon -cp  daemon.jar com.fast.dht.main.DeamonMain
+java -Xmx500m -Xms100m -Duser.timezone=GMT+8 -Dname=Deamon -cp  daemon.jar com.fast.dht.main.DeamonMain
--- DHTSniffer-Daemon/src/main/resources/bootstrap.sh
+++ DHTSniffer-Daemon/src/main/resources/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-java -Dname=Deamon -cp  daemon.jar com.fast.dht.main.DeamonMain
+java -Xmx500m -Xms100m -Duser.timezone=GMT+8 -Dname=Deamon -cp  daemon.jar com.fast.dht.main.DeamonMain
