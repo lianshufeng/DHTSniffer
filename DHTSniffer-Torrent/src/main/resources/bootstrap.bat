4d2e45c2197b387c719be5864fc4e313740f8a99
lianshufeng <251708339@qq.com>
2018-03-27 17:00:42 +0800

    修改默认启动配置

--- DHTSniffer-Torrent/src/main/resources/bootstrap.bat
+++ DHTSniffer-Torrent/src/main/resources/bootstrap.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-java -Xmx1000m -Xms100m -Duser.timezone=GMT+8 -Dname=toTorrent -cp  torrent.jar com.fast.dht.torrent.TorrentMain
+java -Xmx500m -Xms100m -Duser.timezone=GMT+8 -Dname=toTorrent -cp  torrent.jar com.fast.dht.torrent.TorrentMain
--- DHTSniffer-Torrent/src/main/resources/bootstrap.sh
+++ DHTSniffer-Torrent/src/main/resources/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-java -Xmx1000m -Xms100m -Duser.timezone=GMT+8 -Dname=toTorrent -cp  torrent.jar com.fast.dht.torrent.TorrentMain
+java -Xmx500m -Xms100m -Duser.timezone=GMT+8 -Dname=toTorrent -cp  torrent.jar com.fast.dht.torrent.TorrentMain
